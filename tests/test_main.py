5b400b7195130a27b1f93cf98c08ca4d0d374652
Parenek67 <kliment207@gmail.com>
2023-01-16 01:48:45 +0300

    all tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,3 +5,25 @@ api_url = 'http://localhost:8000'
 def test_healthcheck():
     responce = requests.get(f'{api_url}/__health')
     assert responce.status_code == 200
+
+class TestAuth():
+    def test_get_users(self):
+        responce = requests.get(f'{api_url}/v1/users')
+        assert responce.status_code == 200
+        assert len(responce.json()) == 0
+
+    def add_user(self):
+        body = { "name": "Vasya", "login": "vasyan123", "password": "pass123" }
+        responce = requests.post(f'{api_url}/v1/postusers', json = body)
+        assert responce.status_code == 200
+        assert responce.json().get('id') == 0   
+
+    def auth_user(self):
+        responce = requests.get(f'{api_url}/v1/users/vasyan123/pass123')
+        assert responce.status_code == 200
+
+    def test_users_count(self):
+        responce = requests.get(f'{api_url}/v1/users')
+        assert responce.status_code == 200
+        assert len(responce.json()) == 1    
+
